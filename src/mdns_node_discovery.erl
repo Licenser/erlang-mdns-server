1cdc0a90ae9dee6a9a5f63ddfe4fe8e0c72097f4
Heinz N. Gies <heinz@licenser.net>
2013-05-22 16:46:59 +0200

    Added option for selecting a interface.

--- src/mdns_node_discovery.erl
+++ src/mdns_node_discovery.erl
@@ -21,21 +21,21 @@
 %% ------------------------------------------------------------------
 
 -export([start_link/0,
-	 start_link/1]).
+         start_link/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
 %% ------------------------------------------------------------------
 
 -export([init/1,
-	 handle_call/3,
-	 handle_cast/2,
-	 handle_info/2,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
          terminate/2,
-	 code_change/3]).
+         code_change/3]).
 
 -export([advertise/0,
-	 stop/0]).
+         stop/0]).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -58,17 +58,17 @@ stop() ->
 %% ------------------------------------------------------------------
 
 -record(state, {type,
-		domain,
-		port,
-		address,
-		ttl = 120,
-		options = [],
-		socket}).
+                domain,
+                port,
+                address,
+                ttl = 120,
+                options = [],
+                interface,
+                socket}).
 
 init(Parameters) ->
     process_flag(trap_exit, true),
     init(Parameters, #state{}).
-
 
 init([{port, Port} | T], State) ->
     init(T, State#state{port = Port});
@@ -82,12 +82,14 @@ init([{ttl, TTL} | T], State) ->
     init(T, State#state{ttl = TTL});
 init([{options, Options} | T], State) ->
     init(T, State#state{options=Options});
+init([{interface, Iface} | T], State) ->
+    init(T, State#state{interface=Iface});
 init([_ | T], State) ->
     init(T, State);
 init([], #state{port = Port, address = Address} = State) ->
     {ok, Socket} = gen_udp:open(Port, [{reuseaddr, true},
-				       {multicast_if, multicast_if()},
-				       {ip, Address}]),
+                                       {multicast_if, multicast_if(State#state.interface)},
+                                       {ip, Address}]),
     {ok, State#state{socket = Socket}, random_timeout(initial, State)}.
 
 handle_call(advertise, _, State) ->
@@ -121,17 +123,26 @@ random_timeout(initial, _) ->
 random_timeout(announcements, #state{ttl = TTL}) ->
     crypto:rand_uniform(TTL * 500, TTL * 1000).
 
-multicast_if() ->
+multicast_if(Iface) ->
     {ok, Interfaces} = inet:getifaddrs(),
-    multicast_if(Interfaces).
+    multicast_if(Interfaces, Iface).
 
-multicast_if([{_, H} | T]) ->
+multicast_if([{_, H} | T], undefined) ->
     case is_running_multicast_interface(proplists:get_value(flags, H)) andalso proplists:is_defined(addr, H) of
-	true ->
-	    v4(proplists:get_all_values(addr, H));
-	false ->
-	    multicast_if(T)
+        true ->
+            v4(proplists:get_all_values(addr, H));
+        false ->
+            multicast_if(T)
+    end;
+multicast_if([{_, H} | T], IFace) ->
+    case is_running_multicast_interface(proplists:get_value(flags, H)) andalso
+        proplists:get_value(addr, H) =:= IFace of
+        true ->
+            v4(proplists:get_all_values(addr, H));
+        false ->
+            multicast_if(T)
     end.
+
 
 v4([{_, _, _, _} = V4 | _]) ->
     V4;
@@ -140,9 +151,9 @@ v4([_ | T]) ->
 
 is_running_multicast_interface(Flags) ->
     lists:member(up, Flags) andalso
-	lists:member(broadcast, Flags) andalso
-	lists:member(running, Flags) andalso
-	lists:member(multicast, Flags).
+        lists:member(broadcast, Flags) andalso
+        lists:member(running, Flags) andalso
+        lists:member(multicast, Flags).
 
 announce(State) ->
     {ok, Hostname} = inet:gethostname(),
@@ -151,57 +162,57 @@ announce(State) ->
 announce(Hostname, #state{address = Address, port = Port, socket = Socket} = State) ->
     Message = message(Hostname, State),
     gen_udp:send(Socket,
-		 Address,
-		 Port,
-		 inet_dns:encode(Message)).
+                 Address,
+                 Port,
+                 inet_dns:encode(Message)).
 
 message(Hostname, State) ->
     inet_dns:make_msg([{header, header()},
-		       {anlist, answers(Hostname, State)},
-		       {arlist, resources(Hostname, State)}]).
+                       {anlist, answers(Hostname, State)},
+                       {arlist, resources(Hostname, State)}]).
 
 header() ->
     inet_dns:make_header([{id,0},
-			  {qr,true},
-			  {opcode,'query'},
-			  {aa,true},
-			  {tc,false},
-			  {rd,false},
-			  {ra,false},
-			  {pr,false},
-			  {rcode,0}]).
+                          {qr,true},
+                          {opcode,'query'},
+                          {aa,true},
+                          {tc,false},
+                          {rd,false},
+                          {ra,false},
+                          {pr,false},
+                          {rcode,0}]).
 
 answers(Hostname, #state{type = Type, domain = Domain, ttl = TTL} = State) ->
     [inet_dns:make_rr([{type, ptr},
-		       {domain, Type ++ Domain},
-		       {class, in},
-		       {ttl, TTL},
-		       {data, instance(Hostname, State)}
-		      ])].
+                       {domain, Type ++ Domain},
+                       {class, in},
+                       {ttl, TTL},
+                       {data, instance(Hostname, State)}
+                      ])].
 
 resources(Hostname, State) ->
     services(Hostname, State) ++ texts(Hostname, State).
 
 services(Hostname, #state{domain = Domain, ttl = TTL, port=Port} = State) ->
     [inet_dns:make_rr([{domain, instance(Hostname, State)},
-		       {type, srv},
-		       {class, in},
-		       {ttl, TTL},
-		       {data, {0, 0, Port, Hostname ++ Domain}}])].
+                       {type, srv},
+                       {class, in},
+                       {ttl, TTL},
+                       {data, {0, 0, Port, Hostname ++ Domain}}])].
 
 texts(Hostname, #state{ttl = TTL, options = Options} = State) ->
     [inet_dns:make_rr([{domain, instance(Hostname, State)},
-		       {type, txt},
-		       {class, in},
-		       {ttl, TTL},
-		       {data, texts_data(Options)}])].
+                       {type, txt},
+                       {class, in},
+                       {ttl, TTL},
+                       {data, texts_data(Options)}])].
 
 
 texts_data([{Opt, Val} | Options]) ->
     [ ensure_list(Opt) ++ "=" ++ ensure_list(Val) | texts_data(Options)];
 texts_data([]) ->
     [].
-    
+
 instance(Hostname, #state{type = Type, domain = Domain}) ->
     Hostname ++ "." ++ Type ++ Domain.
 
@@ -213,9 +224,3 @@ ensure_list(A) when is_atom(A) ->
     atom_to_list(A);
 ensure_list(L) when is_list(L)->
     L.
-
-
-
-
-
-		       
